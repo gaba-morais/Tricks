9a2a28d64084015986ca22d2ef4aa4e809465910
Salomão <49659169+gabrielsalomao@users.noreply.github.com>
2022-04-27 12:23:25 -0400

    Update transfere_dados_de_uma_tabela_para_outra.sql

--- SQL/transfere_dados_de_uma_tabela_para_outra.sql
+++ SQL/transfere_dados_de_uma_tabela_para_outra.sql
@@ -1,21 +1,21 @@
 -- Exemplo
 DECLARE @MovLoc TABLE (RowId INT, MovimentacaoId INT, LocacaoId INT)
-DECLARE @Counter INT, @TotalCount INT, @MovimentacaoId INT,@LocacaoId INT
+DECLARE @Counter INT, @TotalCount INT, @MovimentacaoId INT, @LocacaoId INT
 
 INSERT INTO @MovLoc
-select ROW_NUMBER() OVER(ORDER BY CodigoDaMovimentacao ASC) AS RowId,CodigoDaMovimentacao as MovimentacaoId, LocacaoId from MovimentacaoDeContasBancarias where LocacaoId is not null 
+select ROW_NUMBER() OVER(ORDER BY CodigoDaMovimentacao ASC) AS RowId,CodigoDaMovimentacao AS MovimentacaoId, LocacaoId FROM MovimentacaoDeContasBancarias WHERE LocacaoId IS NOT NULL 
 
 SET @Counter = 1
  
 SET @TotalCount = (SELECT COUNT(*) FROM @MovLoc)
 
-WHILE (@Counter <=@TotalCount)
+WHILE (@Counter <= @TotalCount)
 BEGIN
     SET @MovimentacaoId = (SELECT MovimentacaoId FROM @MovLoc WHERE RowId = @Counter);
     SET @LocacaoId = (SELECT LocacaoId FROM @MovLoc WHERE RowId = @Counter);
 
-    INSERT INTO MovimentacaoLocacao(LocacaoId,MovimentacaoId)
-	Values(@LocacaoId,@MovimentacaoId);
+    INSERT INTO MovimentacaoLocacao(LocacaoId, MovimentacaoId)
+	VALUES(@LocacaoId, @MovimentacaoId);
  
     SET @Counter = @Counter + 1
     CONTINUE;
